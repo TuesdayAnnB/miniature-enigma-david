1dfc1f197ab0aa1325a78935e0ff70cf30ea9650
TuesdayAnnB <40272506+TuesdayAnnB@users.noreply.github.com>
2024-10-02 02:06:59 +0000

    howdy

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,19 @@
 $(document).ready(function(){
+  let chairStyle = document.getElementById("chair").style;
+  let paintingStyle = document.getElementById("painting").style;
+  let plantStyle = document.getElementById("pottedplant").style;
+  let positions = [ ["30%", "45%", "67%"], ["75%", "45%", "75%"] ];
+  let randPosition = (Math.floor(Math.random() * 3));
   
+  document.getElementById("kaboom").style.left = (positions[0][randPosition]);
+  document.getElementById("kaboom").style.top = (positions[1][randPosition]);
+
     $('#reset').on('click', function(){
-      //move my doge back to the start 
-       $(".furniture").position().left = 800;  
-    })
-    
-    $(".furniture").draggable( 
-    {
-        stop: function() {
-         // alert('dragged'); 
-          
-          if ( $(".furniture").position().left > 100)
-            {
-              //they won
-              $("body").append("<h2>YOU WON</h2> ") 
-            } ); 
-          
-   
-        }
-    }); }
+      document.getElementById("chair").style = chairStyle;
+      document.getElementById("painting").style = paintingStyle;
+      document.getElementById("pottedplant").style = plantStyle;
+
+    });
+
+    $(".furniture").draggable()
+  });
